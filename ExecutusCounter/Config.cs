24061ecc84a6b814810ced0f30b7cf15b38c0496
Kjeld Schmidt <contact@kjeld-schmidt.com>
2020-12-19 20:50:08 +0100

    Added a config file to store the overlay position. Thanks to an new external package, the code is extremely simple and adding more config parameters should be very easy.

---
+++ ExecutusCounter/Config.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ExecutusCounter
+{
+    public class Config
+    {
+        private static readonly string ConfigFolderPath =
+            Hearthstone_Deck_Tracker.Config.AppDataPath + @"\Plugins\ExecutusCounter";
+        private static readonly string ConfigFilePath = ConfigFolderPath + @"\ExecutusCounter.config";
+        public double ExecutusCounterTop = 0;
+        public double ExecutusCounterLeft = 0;
+
+        public void Save()
+        {
+            Directory.CreateDirectory(ConfigFolderPath);
+            File.WriteAllText(ConfigFilePath, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+        
+        private Config() {}
+
+        public static Config Load()
+        {
+            return File.Exists(ConfigFilePath) 
+                ? JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigFilePath)) 
+                : new Config();
+        }
+    }
+}
--- ExecutusCounter/ExecutusCounterPlugin.cs
+++ ExecutusCounter/ExecutusCounterPlugin.cs
@@ -12,17 +12,27 @@ namespace ExecutusCounter
     {
         private ExecutusCounter _counter;
         private ElementalCounterOverlay _counterOverlay;
+        private Config _config;
+        
         public void OnLoad()
         {
             _counterOverlay = new ElementalCounterOverlay();
             Core.OverlayCanvas.Children.Add(_counterOverlay);
 
+            // The overlay will not receive mouse events unless it is added to `_clickableElements`.
+            // This is not currently supported via the api, so reflection is needed.
+            // This was recommended by the developers on the discord.
             (Core.OverlayWindow.GetType()
-                .GetField("_clickableElements", BindingFlags.NonPublic | BindingFlags.Instance)
-                ?.GetValue(Core.OverlayWindow) as List<FrameworkElement>)
+                    .GetField("_clickableElements", BindingFlags.NonPublic | BindingFlags.Instance)
+                    ?.GetValue(Core.OverlayWindow) as List<FrameworkElement>)
                 ?.Add(_counterOverlay);
+            
+            
+            _config = Config.Load();
+            ApplyConfig();
 
             _counter = new ExecutusCounter(_counterOverlay);
+            
             GameEvents.OnGameStart.Add(_counter.GameStart);
             GameEvents.OnGameEnd.Add(_counter.GameEnd);
             GameEvents.OnPlayerPlay.Add(_counter.PlayerPlayed);
@@ -30,9 +40,23 @@ namespace ExecutusCounter
             GameEvents.OnInMenu.Add(_counter.InMenu);
         }
 
+        private void ApplyConfig()
+        {
+            Canvas.SetTop(_counterOverlay, _config.ExecutusCounterTop);
+            Canvas.SetLeft(_counterOverlay, _config.ExecutusCounterLeft);
+        }
+
         public void OnUnload()
         {
+            WriteConfig();
             Core.OverlayCanvas.Children.Remove(_counterOverlay);
+        }
+
+        private void WriteConfig()
+        {
+            _config.ExecutusCounterTop = Canvas.GetTop(_counterOverlay);
+            _config.ExecutusCounterLeft = Canvas.GetLeft(_counterOverlay);
+            _config.Save();
         }
 
         public void OnButtonPress()
@@ -51,7 +75,7 @@ namespace ExecutusCounter
 
         public string ButtonText => "Settings";
         public string Author => "Kjeld Schmidt";
-        public Version Version => new Version("0.3.6");
+        public Version Version => new Version("0.3.7");
         public MenuItem MenuItem => null;
     }
 }
